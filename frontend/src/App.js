b625dd22abaf819dcb8651f7e4ffb01e31ad7883
gamrom <gamrom01@naver.com>
2021-09-22 20:11:59 +0900

    설명코드 업로드 - test

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <div>이 페이지가 뜨면 성공!</div>
-    </div>
-  );
-}
-
-export default App;
---
+++ frontend/src/Main.js
@@ -0,0 +1,12 @@
+import { jsx, css } from '@emotion/react'
+
+function Main() {
+
+  return (
+    <>
+      <div style={{display: "flex"}} className="flex">이 페이지가 뜨면 성공!</div>
+    </>
+  );
+}
+
+export default Main;
