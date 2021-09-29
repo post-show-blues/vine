ce904da64fc3cfe3196952fa99d96a406b359827
gamrom <gamrom01@naver.com>
2021-09-29 21:43:26 +0900

    routing 네이밍 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,14 @@ function App() {
       <Route exact path="/" component={HomeIndex} />
       <Route path="/meetings/new" component={MeetingsNew} />
       <Route path="/meetings/join" component={MeetingsJoin} />
+      <Route path="/meetings" component={HomeIndex} /> {/*  */}
+      <Route path="/search" component={HomeIndex} /> {/* 검색(4개페이지) */}
+      <Route path="/about" component={HomeIndex} /> {/* 소개 */}
+      <Route path="/mypage" component={HomeIndex} /> {/* 마이페이지 */}
+      <Route path="/mypage/edit" component={HomeIndex} /> {/* 마이페이지 수정 */}
+      <Route path="/signup" component={HomeIndex} /> {/* 로그인 */}
+      <Route path="/signin" component={HomeIndex} /> {/* 회원가입 */}
+      <Route path="/participation" component={HomeIndex} /> {/* 회원가입 */}
       {/* 여기에 라우팅 주소 추가해서 만드시면 됩니다 */}
     </div>
   );
