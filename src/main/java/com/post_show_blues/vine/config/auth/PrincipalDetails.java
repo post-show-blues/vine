160bc6eebe954d420e07ab09bf19cef0cf76a332
JM_CHOI_ <chlwjdals98@gmail.com>
2021-10-03 23:34:25 +0900

    JWT 로그인 추가, 세션 로그인 삭제

--- src/main/java/com/post_show_blues/vine/config/auth/PrincipalDetails.java
+++ src/main/java/com/post_show_blues/vine/config/auth/PrincipalDetails.java
@@ -4,27 +4,54 @@ import com.post_show_blues.vine.domain.member.Member;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 
 @Getter
 @Setter
 public class PrincipalDetails implements UserDetails {
 
+    private static final String ROLE_USER = "ROLE_USER";
+
     private Member member;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
 
     public PrincipalDetails(Member member) {
         this.member = member;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    public PrincipalDetails(Member member, Collection<? extends GrantedAuthority> authorities) {
+        this.member = member;
+        this.authorities = authorities;
+    }
+
+
+    public static PrincipalDetails create(Member member) {
+        List<GrantedAuthority> authorities = Collections.
+                singletonList(new SimpleGrantedAuthority(ROLE_USER));
+
+        return new PrincipalDetails(member, authorities);
+    }
+
+    public static PrincipalDetails create(Member member, Map<String, Object> attributes) {
+        PrincipalDetails userPrincipal = PrincipalDetails.create(member);
+        userPrincipal.setAttributes(attributes);
+        return userPrincipal;
     }
 
     public Long getId(){
         return member.getId();
+    }
+
+    public String getEmail(){
+        return member.getEmail();
     }
 
     @Override
@@ -55,5 +82,18 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
     }
 }
---
+++ src/main/java/com/post_show_blues/vine/dto/Auth/TokenDto.java
@@ -0,0 +1,12 @@
+package com.post_show_blues.vine.dto.auth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String token;
+}
